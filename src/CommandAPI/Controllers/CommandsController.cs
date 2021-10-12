a2256842f4ff3915356d78555246608608fd98a2
Bruce <bharris133@hotmail.com>
2021-10-11 19:25:34 -0500

    Initial Commit

---
+++ src/CommandAPI/Controllers/CommandsController.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommandAPI.Controllers
+{
+
+   [Route("api/[controller]")]
+   [ApiController]
+   public class CommandsController:ControllerBase
+   {
+      [HttpGet]
+      public ActionResult<IEnumerable<string>> Get()
+      {
+         return new string[] {"This", "is", "hard", "coded"};
+      }
+   }
+
+}
